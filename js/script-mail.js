521750eb5cec6af91eeb789e30ca2380d13631eb
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-03-13 20:14:48 +0100

    fix btn-play

--- js/script-mail.js
+++ js/script-mail.js
@@ -18,26 +18,38 @@ function checkMail(e) {
 
     e.preventDefault();
     let tutorVerifificato = ''
+    let mail = document.getElementById('mail').value
     for (let i = 0; i < tutorMail.length; i++){
 
 
-        let mail = document.getElementById('mail').value
     
     //    console.log(mail)
         
        // console.log(tutorMail[i])
 
-        if(tutorMail[i] === mail ) {
+        if(tutorMail[i] === mail) {
 
-            tutorVerifificato= tutorMail[i]
-         //   console.log('verificato')
+            tutorVerifificato = tutorMail[i];
+          
         }
-
-
+        
+    }
+    
+    if (tutorVerifificato){
+        check.innerHTML=
+        `<h1 class="text-danger text-center">Benvenuto caro Tutor!</h1>`
+        //console.log('verificato');
     }
 
+    else {
+        check.innerHTML=
 
+        `<h1 class="text-danger text-center">Chisei!? come sei arrivato su questa pagina!?</h1>`
+        //  console.log('non verificato')
 
+    }
+    
+    
 
 
 
