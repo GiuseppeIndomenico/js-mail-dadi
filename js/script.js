ceddadb1e79b2caa08cbfe42b8ada5f3b21ab1ba
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-03-13 17:37:18 +0100

    block#2

--- js/script.js
+++ js/script.js
@@ -1,35 +1,63 @@
 
 
-const tutorMail= [
+const tutorMail = [
     'cleliafradella@boolean.it',
     'marcoacciari@boolean.it',
     'samuelpanicucci@boolean.it'
 ];
 
 
-const checkBtn= document.getElementById('checkBtn')
+const checkBtn = document.getElementById('checkBtn')
+const check = document.getElementById('check')
 
 let mail;
 
-console.log(checkBtn)
+
+
+//console.log(checkBtn)
 
 checkBtn.addEventListener('click', checkMail)
 
-function checkMail (e){
+function checkMail(e) {
 
     e.preventDefault();
 
-   let mail= document.getElementById('mail').value
+    let mail = document.getElementById('mail').value
+    let tutor = ''
 
-   console.log(mail)
+    for (let i = 0; i < tutorMail.length; i++) {
+
+        console.log(mail)
+        console.log(tutorMail[i])
+
+        let tutor = tutorMail[i]
+
+    }
+    if (tutor) {
+
+        check.innerHTML =
+
+            `
+           <h1>Complimenti! Sei un ottimo tutor!</h1>        
+           `
+
+
+
+    } else {
+        check.innerHTML =
+            `
+            <h1>Chi sei!? e come sei arrivato su questa pagina?</h1>   
+            `
+
+
+    }
+
+
 
 
 }
 
 
 
-for (let i = 0; i< tutorMail.length; i++ ) {
 
- 
 
-}
