9fb5af30a8de0df1f205b69f7d150e0c51be80f2
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-03-13 19:39:26 +0100

    restart check-mail

--- js/script-mail.js
+++ js/script-mail.js
@@ -1,6 +1,6 @@
-
-
 const tutorMail = [
+
+
     'cleliafradella@boolean.it',
     'marcoacciari@boolean.it',
     'samuelpanicucci@boolean.it'
@@ -10,10 +10,6 @@ const tutorMail = [
 const checkBtn = document.getElementById('checkBtn')
 const check = document.getElementById('check')
 
-let mail = '';
-
-
-
 //console.log(checkBtn)
 
 checkBtn.addEventListener('click', checkMail)
@@ -21,48 +17,30 @@ checkBtn.addEventListener('click', checkMail)
 function checkMail(e) {
 
     e.preventDefault();
-
-    let mail = document.getElementById('mail').value
-    let correct = ''
-
-
-    for (let i = 0; i < tutorMail.length; i++) {
-
-       console.log(mail)
-       console.log(tutorMail[i])
-
-        if(mail === tutorMail[i]) {
+    let tutorVerifificato = ''
+    for (let i = 0; i < tutorMail.length; i++){
 
 
-            let correct = mail
-            console.log(correct)
+        let mail = document.getElementById('mail').value
+    
+    //    console.log(mail)
+        
+       // console.log(tutorMail[i])
+
+        if(tutorMail[i] === mail ) {
+
+            tutorVerifificato= tutorMail[i]
+         //   console.log('verificato')
         }
 
-        if (correct === tutorMail[i]) {
 
-            check.innerHTML =
-
-                `
-               <h1>Complimenti! Sei un ottimo tutor!</h1>        
-                `
-
-        }
-        // if else (mail = ''){
-
-        //     check.innerHTML =
-
-        //         `<h1> Hmmm...qualcosa è andato storto ritenta!</h1>`
-
-        // }
-        else {
-            check.innerHTML =
-                `
-                <h1>Chi sei!? e come sei arrivato su questa pagina?</h1>   
-                `
-
-
-        }
     }
+
+
+
+
+
+
 
 }
 
